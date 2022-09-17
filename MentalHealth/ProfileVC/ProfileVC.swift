7d1c5c1dea483a4b41b67436e3fb6cbccb0ca593
Dmitriy Holovnia <dmitriy.holovnia@uptech.team>
2022-09-17 15:40:33 +0300

    add default vc

--- MentalHealth/ProfileVC/ProfileVC.swift
+++ MentalHealth/ProfileVC/ProfileVC.swift
@@ -7,22 +7,12 @@
 
 import UIKit
 
-class ProfileVC: UIViewController {
+class ProfileVC: DefaultViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .white
+        titleLabel.text = "Профіль"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
